1fcf2d76ee1f87648fdb94a90b328030acf2fea0
Quadrilateral <114942532+Diamond-The-Quadrilateral@users.noreply.github.com>
2022-12-02 12:46:42 -0500

    Create conCat.java

---
+++ 07-string-puzzles/conCat.java
@@ -0,0 +1,16 @@
+class ConCat {
+  public static void main(String[] args) {
+    System.out.println(conCat("abc","cat"));
+    System.out.println(conCat("dog","cat"));
+    System.out.println(conCat("abc",""));
+  }// end main
+  public static String conCat(String word1, String word2){
+    String conword ="";
+    if((word1!="" && word2 !="")&&(word1.substring(word1.length()-1)).equals(word2.substring(0,1))){
+      conword = word1+word2.substring(1);
+    } else {
+      conword = word1+word2;
+    }
+    return conword;
+  }// end concat
+}// end main class
