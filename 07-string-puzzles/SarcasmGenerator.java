4e09eafb0c1f5d9df517f0d738f6563fa0b1e2dd
Quadrilateral <114942532+Diamond-The-Quadrilateral@users.noreply.github.com>
2022-12-02 12:55:13 -0500

    Create SarcasmGenerator.java

---
+++ 07-string-puzzles/SarcasmGenerator.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(sarcasmGenerator("Hello World"));
+  }
+  public static String sarcasmGenerator(String s){
+  String newString = "";
+  for(int i = 0; i < s.length(); i++){
+    String character = s.substring(i,i+1);
+  if(i%2 == 0){
+    newString += character.toLowerCase();
+  }else{
+    newString +=character.toUpperCase();
+  }
+  //if(s.substring(i-1)
+  }// end for loop
+  return newString;  
+  }// end sarcGen
+}// end main class
